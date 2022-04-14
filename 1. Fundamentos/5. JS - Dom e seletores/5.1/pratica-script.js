a84e2439f7f512700b00b4a701e286e492959b2f
Laura Lana <lauramarqueslana@gmail.com>
2022-04-14 11:35:22 -0300

    Adiciona exercicio 6 com resolução que funciona

--- 1. Fundamentos/5. JS - Dom e seletores/5.1/pratica-script.js
+++ 1. Fundamentos/5. JS - Dom e seletores/5.1/pratica-script.js
@@ -1,23 +1,25 @@
 const paragraph = document.getElementById("paragraph");
-      paragraph.style.color = "red";
+paragraph.style.color = "red";
 
-     document.getElementById("page-title").innerText = "Harry Potter and the Prisoner of Azkaban"; 
+document.getElementById("page-title").innerText = "Harry Potter and the Prisoner of Azkaban";
 
-     document.getElementById("second-paragraph").innerText = "Becouse ...";
-     document.getElementById("second-paragraph").style.color ="#C78D8B";
+document.getElementById("second-paragraph").innerText = "Becouse ...";
+document.getElementById("second-paragraph").style.color = "#C78D8B";
 
-     document.getElementById("subtitle").innerText = "Why HP3 is my favorite Harry Potter movie ?";
-    
-     document.getElementsByClassName('paragraphClass')[0].innerText = "I solemnly swear that I am up to no good";
-     document.getElementsByClassName('paragraphClass')[0].style.backgroundColor = "whitesmoke";
-     document.getElementsByClassName('paragraphClass')[0].style.color = "black";
-     document.getElementsByClassName('paragraphClass')[0].style.textAlign = "center";
-     document.getElementsByClassName('paragraphClass')[0].style.margin = "5%";
+document.getElementById("subtitle").innerText = "Why HP3 is my favorite Harry Potter movie ?";
 
-     document.getElementsByTagName("h4")[0].style.color = "#BD9596";
+document.getElementsByClassName('paragraphClass')[0].innerText = "I solemnly swear that I am up to no good";
+document.getElementsByClassName('paragraphClass')[0].style.backgroundColor = "whitesmoke";
+document.getElementsByClassName('paragraphClass')[0].style.color = "black";
+document.getElementsByClassName('paragraphClass')[0].style.textAlign = "center";
+document.getElementsByClassName('paragraphClass')[0].style.margin = "5%";
 
-     document.getElementsByClassName('paragraphClass')[2].innerText = "Mischief managed.";
-     document.getElementsByClassName('paragraphClass')[2].style.backgroundColor = "whitesmoke";
-     document.getElementsByClassName('paragraphClass')[2].style.color = "black";
-     document.getElementsByClassName('paragraphClass')[2].style.textAlign = "center";
-     document.getElementsByClassName('paragraphClass')[2].style.marginTop = "5%";
+document.getElementsByTagName("h4")[0].style.color = "#BD9596";
+
+document.getElementsByClassName('paragraphClass')[2].innerText = "Mischief managed.";
+document.getElementsByClassName('paragraphClass')[2].style.backgroundColor = "whitesmoke";
+document.getElementsByClassName('paragraphClass')[2].style.color = "black";
+document.getElementsByClassName('paragraphClass')[2].style.textAlign = "center";
+document.getElementsByClassName('paragraphClass')[2].style.marginTop = "5%";
+
+  
