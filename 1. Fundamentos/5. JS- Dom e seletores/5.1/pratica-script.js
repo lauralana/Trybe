480baef422217870988b3464a3e6fe641d53b255
Laura Lana <lauramarqueslana@gmail.com>
2022-04-13 22:21:53 -0300

    Adiciona prática de fixação dia 1 do bloco 5

---
+++ 1. Fundamentos/5. JS- Dom e seletores/5.1/pratica-script.js
@@ -0,0 +1,23 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+     document.getElementById("page-title").innerText = "Harry Potter and the Prisoner of Azkaban"; 
+
+     document.getElementById("second-paragraph").innerText = "Becouse ...";
+     document.getElementById("second-paragraph").style.color ="#C78D8B";
+
+     document.getElementById("subtitle").innerText = "Why HP3 is my favorite Harry Potter movie ?";
+    
+     document.getElementsByClassName('paragraphClass')[0].innerText = "I solemnly swear that I am up to no good";
+     document.getElementsByClassName('paragraphClass')[0].style.backgroundColor = "whitesmoke";
+     document.getElementsByClassName('paragraphClass')[0].style.color = "black";
+     document.getElementsByClassName('paragraphClass')[0].style.textAlign = "center";
+     document.getElementsByClassName('paragraphClass')[0].style.margin = "5%";
+
+     document.getElementsByTagName("h4")[0].style.color = "#BD9596";
+
+     document.getElementsByClassName('paragraphClass')[2].innerText = "Mischief managed.";
+     document.getElementsByClassName('paragraphClass')[2].style.backgroundColor = "whitesmoke";
+     document.getElementsByClassName('paragraphClass')[2].style.color = "black";
+     document.getElementsByClassName('paragraphClass')[2].style.textAlign = "center";
+     document.getElementsByClassName('paragraphClass')[2].style.marginTop = "5%";
